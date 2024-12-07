1e16e852fa177405e46df7ad8e65ab25f6ad1f9d
Sy-Yamaguchi-cat <sei.rm0307@gmail.com>
2024-12-08 00:04:25 +0900

    improve ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+import path from "path";
+
 export default defineConfig({
   build: {
     outDir: "public",
   },
   publicDir: false,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "!": path.resolve(__dirname, "./config"),
+    },
+  },
   plugins: [react()],
 });
